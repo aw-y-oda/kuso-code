660bf47dc39bbc162c16ec70dd36b85bef4401dd
aw-y-tsunoka <y_tsunoka@active.co.jp>
2025-11-25 06:07:28 +0900

    解答sample

--- sample2.ts
+++ sample2.ts
@@ -37,32 +37,44 @@ function processOrder(apple: Apple, orange: Orange, grape: Grape, currentDate: D
   const grapeRottenDate = new Date(currentDate);
   grapeRottenDate.setDate(grapeRottenDate.getDate() + 7);
 
+  // わかりきっている条件で区切ってreturnする
+  // 対象となるデータ範囲が大きいものを先に書くと読みやすい
+  // 仕様単位でまとまっていると良いが、条件の漏れに注意
   if (apple.isRotten(currentDate) || orange.isRotten(currentDate) || grape.isRotten(currentDate)) {
     apple.isNeedToOrder = true;
     orange.isNeedToOrder = true;
     grape.isNeedToOrder = true;
-  } else if (apple.num + orange.num + grape.num >= 6) {
+    return;
+  }
+  
+  if (apple.num + orange.num + grape.num >= 6) {
     apple.isNeedToDeliver = true;
     orange.isNeedToDeliver = true;
     grape.isNeedToDeliver = true;
-  } else if (apple.isRotten(orangeRottenDate) && orange.isRotten(orangeRottenDate)) {
+    return;
+  }
+  if (apple.isRotten(orangeRottenDate) && orange.isRotten(orangeRottenDate)) {
     apple.isNeedToDiscard = true;
     orange.isNeedToDiscard = true;
     grape.isNeedToDeliver = true;
-  } else if (grape.isRotten(orangeRottenDate) && orange.isRotten(orangeRottenDate)) {
+    return;
+  }
+  if (grape.isRotten(orangeRottenDate) && orange.isRotten(orangeRottenDate)) {
     grape.isNeedToOrder = true;
     orange.isNeedToOrder = true;
     apple.isNeedToDiscard = true;
-  } else if (apple.isRotten(appleRottenDate)) {
+    return;
+  }
+  if (apple.isRotten(appleRottenDate)) {
     orange.isNeedToDeliver = true;
     grape.isNeedToDeliver = true;
-  } else {
-    if (orange.isRotten(orangeRottenDate)) {
-      orange.isNeedToOrder = true;
-    }
-    if (grape.isRotten(grapeRottenDate)) {
-      grape.isNeedToDiscard = true;
-    }
+    return;
+  }
+  if (orange.isRotten(orangeRottenDate)) {
+    orange.isNeedToOrder = true;
+  }
+  if (grape.isRotten(grapeRottenDate)) {
+    grape.isNeedToDiscard = true;
   }
 
 }
